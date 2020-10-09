f9fe069fc498092b8363804894f3132416c49d7f
kghugo <howtostudycantonese@gmail.com>
2020-10-10 00:53:14 +0800

    :fire: Remove code for checking undefined with map

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,6 @@ const {
   safeMap
 } = require('../output/Helper/index.js')
 
-console.log(safeMap)
-
-console.log(
-  safeMap(n => n * n)([1, 2, 3])
-)
 
 function Faq ({ faqs, attr }) {
   const faqArray = safeMap(
@@ -19,14 +14,6 @@ function Faq ({ faqs, attr }) {
   )(
     faqs
   )
-
-  // faqs.map(({
-  //   question,
-  //   answer
-  // }) =>
-  //   <Question question={question} answer={answer} attr={attr}/>
-  // )
-  // const faqArray = safeMap([1, 2, 3])(n => n * n)
 
   return (
     <div itemscope="itemscope" itemtype="https://schema.org/FAQPage" {...attr.Container}>
--- src/utilities/Helper.purs
+++ src/utilities/Helper.purs
@@ -4,13 +4,8 @@ import Prelude
 import Data.Maybe
 import Data.Array
 import Data.Functor
-import Foreign
 
 safeMap :: forall a b. (a -> b) -> Array a -> Array b
-safeMap fn arr
-  | (isUndefined (unsafeToForeign arr)) = map fn []
-  | otherwise = map fn arr
-
--- safeMap fn arr = map fn arr
+safeMap fn arr = map fn arr
 
 -- Handle case when arr is undefined
