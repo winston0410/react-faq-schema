e5edbff246183822c316a157fc18d2d7e20a21ba
kghugo <howtostudycantonese@gmail.com>
2020-10-11 01:37:08 +0800

    :bug: Import React into the components

--- src/partials/Answer.jsx
+++ src/partials/Answer.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 function Answer ({ answer, attr, tagName }) {
   const TagName = tagName.Answer.tagName
   const TextTagName = tagName.AnswerText.tagName
--- src/partials/Question.jsx
+++ src/partials/Question.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Answer from './Answer.jsx'
 
 function Question ({ question, answer, attr, tagName }) {
